7e6877a18d7cf0d2dc404548277e2708918df689
deep patel <nirmal@krishnasoftware.com>
2023-07-28 17:43:32 +0530

    deep

--- lib/components/screen/screenhome.dart
+++ lib/components/screen/screenhome.dart
@@ -114,8 +114,8 @@ class _homescreenState extends State<homescreen> {
                   contentPadding: EdgeInsets.all(5),
                   tileColor:  index % 2 == 0 ? Colors.white : Colors.white,
                   leading: CircleAvatar(
-
-                    child:Image.network("http://e-gam.com/GKA/Logo/GKA%20logo.jpg"),
+                    radius: 30,
+                    backgroundImage: NetworkImage("http://e-gam.com/GKA/Logo/GKA%20logo.jpg"),
                   ) ,
                   title: Text('${index + 1}',),
                   subtitle:Text(result[index].name),

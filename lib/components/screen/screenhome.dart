e9531f6dac5bf9049394d7913e50e9aec9acc280
deep patel <nirmal@krishnasoftware.com>
2023-08-02 15:08:14 +0530

    final1

--- lib/components/screen/screenhome.dart
+++ lib/components/screen/screenhome.dart
@@ -11,13 +11,12 @@ class homescreen extends StatefulWidget {
   State<homescreen> createState() => _homescreenState();
 }
 
-class _homescreenState extends State<homescreen> {
-  List<Result> result=[];
-  bool loading=false;
-  int off=0;
-  bool visiblebtn=true;
-  ScrollController scrollController=ScrollController();
-
+class _homescreenState extends State<homescreen>{
+  List<Result> result = [];
+  bool loading = false;
+  int off =0;
+  bool visiblebtn = true;
+  ScrollController scrollController = ScrollController();
 
   @override
   void initState() {
@@ -26,116 +25,246 @@ class _homescreenState extends State<homescreen> {
     handleNext();
   }
 
-
-
-  void handleNext(){
-    scrollController.addListener(() async{
-      if(scrollController.position.maxScrollExtent ==
-          scrollController.position.pixels){
-
+  void handleNext() {
+    scrollController.addListener(() async {
+      if (scrollController.position.maxScrollExtent ==
+          scrollController.position.pixels) {
         setState(() {
-          loading=true;
+          loading = true;
         });
         fetchData(off);
-
       }
     });
   }
 
-  Future<void>  fetchData(paraoffset) async{
-    var url="http://e-gam.com/GKARESTAPI/welcomePage?off=${paraoffset}&lim=10";
+  Future<void> fetchData(paraoffset) async {
+
+    var url =
+        "http://e-gam.com/GKARESTAPI/welcomePage?off=${paraoffset}&lim=25";
     print(url);
-    var uri=Uri.parse(url);
-    var response=await http.get(uri);
-    if(response.statusCode==200){
-      Welcome welcome=Welcome.fromJson(json.decode(response.body));
-      result=result + welcome.results;
-      int localOffset=off+10;
+    var uri = Uri.parse(url);
+    var response = await http.get(uri);
+    if (response.statusCode == 200) {
+      Welcome welcome = Welcome.fromJson(json.decode(response.body));
+      result = result + welcome.results;
+      int localOffset = off + 25;
       setState(() {
         result;
-        loading=false;
-        off=localOffset;
+        loading = false;
+        off = localOffset;
       });
+    }else{
+      print("error===============huhuj");
     }
   }
+
   var status = 0;
-
-  popupMenu(id){
-    print(id);
-    return showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return Container(
-          height: 200,
-          color: Colors.amber,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const Text('Modal BottomSheet'),
-                ElevatedButton(
-                  child: const Text('Close BottomSheet'),
-                  onPressed:() {
-                    Navigator.pop(context);
-
-                  },
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
 
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      body:ListView.builder(
+    return Scaffold(
+      body: ListView.builder(
         controller: scrollController,
-        itemCount: loading ? result.length+1 : result.length,
+        itemCount: loading ? result.length + 1 : result.length,
         itemBuilder: (context, index) {
-          if(index < result.length){
+          if (index < result.length) {
             return Card(
               shape: Border.all(color: Colors.grey),
               elevation: 8,
               child: InkWell(
-                onTap: (){
-                  popupMenu(result[index].id);
+                onTap: () {
+
+                  // result[index].id
                 },
                 child: ListTile(
-
-                  trailing:IconButton(icon: Icon(Icons.ads_click_sharp,),
-                    onPressed: () {
-                      popupMenu(result[index].id);
-                    },),
-                  contentPadding: EdgeInsets.all(5),
-                  tileColor:  index % 2 == 0 ? Colors.white : Colors.white,
                   leading: CircleAvatar(
                     radius: 30,
-                    backgroundImage: NetworkImage("http://e-gam.com/GKA/Logo/GKA%20logo.jpg"),
-                  ) ,
-                  title: Text('${index + 1}',),
-                  subtitle:Text(result[index].name),
+                    backgroundImage: NetworkImage(
+                        "http://e-gam.com/GKA/Logo/GKA%20logo.jpg"),
+                  ),
+                  title: Text(
+                    '${index + 1}',
+                  ),
+                  subtitle: Text(result[index].name.toString()),
+                  // children: [
+                  //   // ExpansionTile(
+                  //   //   title: Text("Basic Information"),
+                  //   //   children: [
+                  //   //     Card(
+                  //   //       child: Column(
+                  //   //       children: [
+                  //   //         Row(
+                  //   //           children: [
+                  //   //             Text('Address1'),
+                  //   //             Expanded(child: Text(result[index].address)),
+                  //   //           ],
+                  //   //         ),
+                  //   //         Row(
+                  //   //           children: [
+                  //   //             Text('Address2'),
+                  //   //             Expanded(child: Text(result[index].address1)),
+                  //   //           ],
+                  //   //         ),
+                  //   //         Row(
+                  //   //           children: [
+                  //   //             Text('Country'),
+                  //   //             Expanded(child: Text(result[index].country.toString())),
+                  //   //           ],
+                  //   //         ),
+                  //   //         Row(
+                  //   //           children: [
+                  //   //             Text('State'),
+                  //   //             Expanded(child: Text(result[index].state)),
+                  //   //           ],
+                  //   //         ),
+                  //   //         Row(
+                  //   //           children: [
+                  //   //             Text('City'),
+                  //   //             Expanded(child: Text(result[index].cities)),
+                  //   //           ],
+                  //   //         ),
+                  //   //         Row(
+                  //   //           children: [
+                  //   //             Text('Zip/Pin'),
+                  //   //             Expanded(child: Text(result[index].pin)),
+                  //   //           ],
+                  //   //         ),
+                  //   //
+                  //   //       ],
+                  //   //     ),)
+                  //   //   ],
+                  //   // ),
+                  //   // ExpansionTile(
+                  //   //   title: Text("Contact Information"),
+                  //   //   children: [
+                  //   //     Card(
+                  //   //       child: Column(
+                  //   //         children: [
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Country Code'),
+                  //   //               Expanded(child: Text("+1")),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Home Phone'),
+                  //   //               Expanded(child: Text(result[index].phone)),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Mobile'),
+                  //   //               Expanded(child: Text(result[index].mob)),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Email'),
+                  //   //               Expanded(child: Text(result[index].email)),
+                  //   //             ],
+                  //   //           ),
+                  //   //
+                  //   //         ],
+                  //   //       ),)
+                  //   //   ],
+                  //   // ),
+                  //   // ExpansionTile(
+                  //   //   title: Text("Businees Details"),
+                  //   //   children: [
+                  //   //     Card(
+                  //   //       child: Column(
+                  //   //         children: [
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Website'),
+                  //   //               Expanded(child: Text(result[index].bWebsite.toString())),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Type of Businees'),
+                  //   //               Expanded(child: Text(result[index].bDetail.toString())),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Location'),
+                  //   //               Expanded(child: Text(result[index].bLocation.toString())),
+                  //   //             ],
+                  //   //           ),
+                  //   //         ],
+                  //   //       ),)
+                  //   //   ],
+                  //   // ),
+                  //   // ExpansionTile(
+                  //   //   title: Text("Family Details"),
+                  //   //   children: [
+                  //   //     Card(
+                  //   //       child: Column(
+                  //   //         children: [
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Parents'),
+                  //   //               Expanded(child: Text(result[index].parent)),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Spouse Parents'),
+                  //   //               Expanded(child: Text(result[index].sParents)),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Native:City/Country'),
+                  //   //               Expanded(child: Text(result[index].natCity.toString())),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Name of Samajik Sanstha Involved'),
+                  //   //               Expanded(child: Text(result[index].sSanstha)),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Extra1'),
+                  //   //               Expanded(child: Text(result[index].ext1.toString())),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('Extra2'),
+                  //   //               Expanded(child: Text(result[index].ext2)),
+                  //   //             ],
+                  //   //           ),
+                  //   //           Row(
+                  //   //             children: [
+                  //   //               Text('About Family'),
+                  //   //               Expanded(child: Text(result[index].aboutFamily)),
+                  //   //             ],
+                  //   //           ),
+                  //   //
+                  //   //         ],
+                  //   //       ),)
+                  //   //   ],
+                  //   // ),
+                  //   Text('hello'),
+                  // ],
                 ),
               ),
             );
-          }else{
-            return const Center(child: CircularProgressIndicator(
+          } else {
+            return const Center(
+                child: CircularProgressIndicator(
               color: Colors.deepOrange,
-
             ));
           }
-
-
         },
-
-
       ),
       floatingActionButton: FloatingActionButton(
-
         onPressed: () {
           if (scrollController.hasClients) {
             final position = scrollController.position.minScrollExtent;
@@ -147,7 +276,6 @@ class _homescreenState extends State<homescreen> {
           }
         },
         isExtended: true,
-
         tooltip: "Scroll to Bottom",
         child: Icon(Icons.arrow_circle_up),
       ),
--- lib/models/welcomejson.dart
+++ lib/models/welcomejson.dart
@@ -1,7 +1,13 @@
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromJson(jsonString);
 
 import 'dart:convert';
+
 Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
+
 String welcomeToJson(Welcome data) => json.encode(data.toJson());
+
 class Welcome {
   String next;
   String previous;
@@ -31,25 +37,287 @@ class Welcome {
 }
 
 class Result {
+  String userApprov;
+  Country country;
+  String mob;
+  BDetail bDetail;
   String address;
+  String email;
+  Cities cities;
+  String parent;
+  String address1;
+  String sSanstha;
+  String natCity;
+  String aboutFamily;
+  BLocation bLocation;
+  String sParents;
+  String pin;
+  String phone;
+  List<Familyinfo> familyinfo;
   String name;
+  BWebsite bWebsite;
   String id;
+  State1 state;
+  String ext2;
+  Ext1 ext1;
 
   Result({
+    required this.userApprov,
+    required this.country,
+    required this.mob,
+    required this.bDetail,
     required this.address,
+    required this.email,
+    required this.cities,
+    required this.parent,
+    required this.address1,
+    required this.sSanstha,
+    required this.natCity,
+    required this.aboutFamily,
+    required this.bLocation,
+    required this.sParents,
+    required this.pin,
+    required this.phone,
+    required this.familyinfo,
     required this.name,
+    required this.bWebsite,
     required this.id,
+    required this.state,
+    required this.ext2,
+    required this.ext1,
   });
 
   factory Result.fromJson(Map<String, dynamic> json) => Result(
+    userApprov: json["User_Approv"],
+    country: countryValues.map[json["country"]]!,
+    mob: json["Mob"],
+    bDetail: bDetailValues.map[json["B_Detail"]]!,
     address: json["address"],
+    email: json["Email"],
+    cities: citiesValues.map[json["cities"]]!,
+    parent: json["Parent"],
+    address1: json["address1"],
+    sSanstha: json["s_sanstha"],
+    natCity: json["Nat_City"],
+    aboutFamily: json["About_Family"],
+    bLocation: bLocationValues.map[json["B_location"]]!,
+    sParents: json["S_Parents"],
+    pin: json["pin"],
+    phone: json["Phone"],
+    familyinfo: List<Familyinfo>.from(json["familyinfo"].map((x) => Familyinfo.fromJson(x))),
     name: json["name"],
+    bWebsite: bWebsiteValues.map[json["B_Website"]]!,
     id: json["id"],
+    state: stateValues.map[json["state"]]!,
+    ext2: json["ext2"],
+    ext1: ext1Values.map[json["ext1"]]!,
   );
 
   Map<String, dynamic> toJson() => {
+    "User_Approv": userApprov,
+    "country": countryValues.reverse[country],
+    "Mob": mob,
+    "B_Detail": bDetailValues.reverse[bDetail],
     "address": address,
+    "Email": email,
+    "cities": citiesValues.reverse[cities],
+    "Parent": parent,
+    "address1": address1,
+    "s_sanstha": sSanstha,
+    "Nat_City": natCity,
+    "About_Family": aboutFamily,
+    "B_location": bLocationValues.reverse[bLocation],
+    "S_Parents": sParents,
+    "pin": pin,
+    "Phone": phone,
+    "familyinfo": List<dynamic>.from(familyinfo.map((x) => x.toJson())),
     "name": name,
+    "B_Website": bWebsiteValues.reverse[bWebsite],
     "id": id,
+    "state": stateValues.reverse[state],
+    "ext2": ext2,
+    "ext1": ext1Values.reverse[ext1],
   };
+}
+
+enum BDetail {
+  EMPTY,
+  IT,
+  TESTING_ONLY
+}
+
+final bDetailValues = EnumValues({
+  "": BDetail.EMPTY,
+  "it": BDetail.IT,
+  "testing only": BDetail.TESTING_ONLY
+});
+
+enum BLocation {
+  EMPTY,
+  SURAT001,
+  SURAT_KIM
+}
+
+final bLocationValues = EnumValues({
+  "": BLocation.EMPTY,
+  "surat001": BLocation.SURAT001,
+  "surat kim": BLocation.SURAT_KIM
+});
+
+enum BWebsite {
+  EMPTY,
+  NIRMALPATEL0531_GMAIL_COM,
+  WWW_DEMO_ORG
+}
+
+final bWebsiteValues = EnumValues({
+  "": BWebsite.EMPTY,
+  "nirmalpatel0531@gmail.com": BWebsite.NIRMALPATEL0531_GMAIL_COM,
+  "www.demo.org": BWebsite.WWW_DEMO_ORG
+});
+
+enum Cities {
+  BEACH,
+  BREWTON,
+  COLUMBUS,
+  CUMMING,
+  EDISON,
+  KENNER,
+  LITTLE_ROCK,
+  LYNNWOOD,
+  NEWNAN,
+  NEW_BERN,
+  SELECT,
+  WEST_COLUMBIA
+}
+
+final citiesValues = EnumValues({
+  "Beach": Cities.BEACH,
+  "Brewton": Cities.BREWTON,
+  "Columbus": Cities.COLUMBUS,
+  "Cumming": Cities.CUMMING,
+  "Edison": Cities.EDISON,
+  "Kenner": Cities.KENNER,
+  "Little Rock": Cities.LITTLE_ROCK,
+  "Lynnwood": Cities.LYNNWOOD,
+  "Newnan": Cities.NEWNAN,
+  "New Bern": Cities.NEW_BERN,
+  "SELECT": Cities.SELECT,
+  "West Columbia": Cities.WEST_COLUMBIA
+});
+
+enum Country {
+  CANADA,
+  UNITED_STATES
+}
+
+final countryValues = EnumValues({
+  "Canada": Country.CANADA,
+  "United States": Country.UNITED_STATES
+});
+
+enum Ext1 {
+  BARDOLI,
+  EMPTY
+}
+
+final ext1Values = EnumValues({
+  "Bardoli": Ext1.BARDOLI,
+  "": Ext1.EMPTY
+});
+
+class Familyinfo {
+  String phone;
+  String dob;
+  String name;
+  Occ occ;
+  String email;
+  String relation;
+
+  Familyinfo({
+    required this.phone,
+    required this.dob,
+    required this.name,
+    required this.occ,
+    required this.email,
+    required this.relation,
+  });
+
+  factory Familyinfo.fromJson(Map<String, dynamic> json) => Familyinfo(
+    phone: json["phone"],
+    dob: json["dob"],
+    name: json["name"],
+    occ: occValues.map[json["occ"]]!,
+    email: json["email"],
+    relation: json["relation"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "phone": phone,
+    "dob": dob,
+    "name": name,
+    "occ": occValues.reverse[occ],
+    "email": email,
+    "relation": relation,
+  };
+}
+
+enum Occ {
+  BUSINESS,
+  ELECTRIC_ENGINEER,
+  EMPTY,
+  HOUSEWIFE,
+  IT_ENGINEER,
+  RETIRED,
+  STUDENT
+}
+
+final occValues = EnumValues({
+  "Business": Occ.BUSINESS,
+  "Electric Engineer": Occ.ELECTRIC_ENGINEER,
+  "": Occ.EMPTY,
+  "Housewife": Occ.HOUSEWIFE,
+  "IT Engineer": Occ.IT_ENGINEER,
+  "Retired ": Occ.RETIRED,
+  "student": Occ.STUDENT
+});
+
+enum State1 {
+  ALABAMA,
+  ARKANSAS,
+  FLORIDA,
+  GEORGIA,
+  LOUISIANA,
+  NEW_JERSEY,
+  NORTH_CAROLINA,
+  OHIO,
+  SELECT,
+  SOUTH_CAROLINA,
+  WASHINGTON
+}
+
+final stateValues = EnumValues({
+  "Alabama": State1.ALABAMA,
+  "Arkansas": State1.ARKANSAS,
+  "Florida": State1.FLORIDA,
+  "Georgia": State1.GEORGIA,
+  "Louisiana": State1.LOUISIANA,
+  "New Jersey": State1.NEW_JERSEY,
+  "North Carolina": State1.NORTH_CAROLINA,
+  "Ohio": State1.OHIO,
+  "SELECT": State1.SELECT,
+  "South Carolina": State1.SOUTH_CAROLINA,
+  "Washington": State1.WASHINGTON
+});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
 }
